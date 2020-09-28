d4210a18bbb0a31103a6a2eaf73b872447befe9b
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2020-09-28 17:14:04 +0300

    extra annotation

--- src/main/kotlin/com/demo/kotlinintro/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/demo/kotlinintro/handler/GlobalExceptionHandler.kt
@@ -5,7 +5,6 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -18,7 +17,6 @@ import javax.validation.ConstraintViolationException
 
 
 @ControllerAdvice
-@Validated
 public class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     override fun handleMethodArgumentNotValid(ex: MethodArgumentNotValidException, headers: HttpHeaders, status: HttpStatus, request: WebRequest): ResponseEntity<Any> {
