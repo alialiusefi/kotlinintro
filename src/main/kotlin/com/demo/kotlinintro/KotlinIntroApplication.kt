ca5dca266be41ae5428ec414bd00729a2d9a9a8b
Ali Al-Iusefi <Ali_Al-Iusefi@epam.com>
2020-09-29 22:31:36 +0300

    extra empty line

--- src/main/kotlin/com/demo/kotlinintro/KotlinIntroApplication.kt
+++ src/main/kotlin/com/demo/kotlinintro/KotlinIntroApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class KotlinIntroApplication
 
 fun main(args: Array<String>) {
-	runApplication<KotlinIntroApplication>(*args)
+    runApplication<KotlinIntroApplication>(*args)
 }
--- src/main/kotlin/com/demo/kotlinintro/exception/ResourceNotFoundException.kt
+++ src/main/kotlin/com/demo/kotlinintro/exception/ResourceNotFoundException.kt
@@ -1,7 +1,4 @@
 package com.demo.kotlinintro.exception
 
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-class ResourceNotFoundException(message: String = "The resource that you requested was not found") : Exception(message)
+class ResourceNotFoundException(message: String) : Exception(message)
