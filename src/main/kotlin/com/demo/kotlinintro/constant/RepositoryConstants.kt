6b0075b18b569f10a6ad41da37deb586c7cbb585
Ali_Al-Iusefi <alighanem99@live.com>
2020-10-08 12:54:11 +0300

    redundant constants

--- src/main/kotlin/com/demo/kotlinintro/constant/RepositoryConstants.kt
+++ src/main/kotlin/com/demo/kotlinintro/constant/RepositoryConstants.kt
@@ -1,9 +1,5 @@
 package com.demo.kotlinintro.constant
 
 object RepositoryConstants {
-    const val FULLNAME = "fullName"
     const val EMAIL = "email"
-    const val DATEOFBIRTH = "dateOfBirth"
-    const val ACTIVE = "active"
-    const val YEARENROLLED = "yearEnrolled"
 }
